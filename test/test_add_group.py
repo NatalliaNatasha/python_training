57ac36677ff1e6224707da6950cd45b86d892f29
Natalia Zen <natallia_zen@senla.eu>
2024-07-09 22:00:45 +0300

    вспомогательные функции для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,16 +11,16 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.init_group_creation()
-    app.fill_group_form(Group(name = "ggg", header = "new header", footer = "new footer"))
-    app.submit_group_creation()
+    app.group_creation.init_group_creation()
+    app.group_creation.fill_group_form(Group(name = "ggg", header = "new header", footer = "new footer"))
+    app.group_creation.submit_group_creation()
     app.session.logout()
 
 def test_add_empty_group(app):
      app.session.login( username="admin",password= "secret")
-     app.init_group_creation()
-     app.fill_group_form(Group(name = "", header = "", footer = ""))
-     app.submit_group_creation()
+     app.group_creation.init_group_creation()
+     app.group_creation.fill_group_form(Group(name = "", header = "", footer = ""))
+     app.group_creation.submit_group_creation()
      app.session.logout()
 
 
