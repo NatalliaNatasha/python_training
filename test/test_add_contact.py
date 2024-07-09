1b071a004a03a4fbd1c9168e01b87fe48499c490
Natalia Zen <natallia_zen@senla.eu>
2024-07-09 22:21:30 +0300

    Вынесение в отдельные функции

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,7 +11,7 @@ def app(request):
 
 def test_add_contact(app):
      app.session.login( username="admin", password="secret")
-     app.create_contact(Contact( firstname="first", middlename="middle", lastname="last", nickname="user", title="new",
+     app.contact_creation.create_contact(Contact( firstname="first", middlename="middle", lastname="last", nickname="user", title="new",
                             companyname="super", address="country", home="hone", mobilephone="888", work="tester",
                             fax="7744", email="ffff@ddd.com", email2="ddddd@yy.com", email3="ddddq@tft.com",
                             page="page", byear="2010", ayear="2000", bday="1", bmonth="December", aday="1",
