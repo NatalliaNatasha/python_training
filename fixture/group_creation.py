f01c1f99df92758ff4e254118976757383842930
Natalia Zen <natallia_zen@senla.eu>
2024-07-13 14:37:12 +0300

    new tests

--- fixture/group_creation.py
+++ fixture/group_creation.py
@@ -29,6 +29,13 @@ class CreationGroup:
        wd.find_element_by_name("submit").click()
        self.return_to_group_page()
 
+
+    def submit_group_edit(self):
+       wd = self.app.wd
+       wd.find_element_by_name("update").click()
+       self.return_to_group_page()
+
+
     def return_to_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
@@ -51,7 +58,7 @@ class CreationGroup:
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("update").click()
+
 
 
 
--- test/test_edit_whole_group.py
+++ test/test_edit_whole_group.py
@@ -1,9 +1,9 @@
-from model.group_edit import Groupedit
+from model.group import Group
 
-def test_add_group(app):
+def edit_group(app):
     app.session.login(username="admin", password="secret")
     app.group_creation.open_groups_page()
     app.group_creation.init_edit_first_group()
-    app.group_creation.fill_edited_group(Groupedit(name=" again"))
+    app.group_creation.fill_group_form(Group(name="edit", header="old", footer="group"))
     app.group_creation.submit_group_edit()
     app.session.logout()
