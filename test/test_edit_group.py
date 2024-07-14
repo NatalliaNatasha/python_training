a6e27ef245ab3e3e25471365ed8e4b9127c2327a
Natalia Zen <natallia_zen@senla.eu>
2024-07-14 22:20:34 +0300

    new tests

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,6 @@
 from model.group import Group
 
-def edit_group(app):
+def test_edit_group(app):
     app.session.login(username="admin", password="secret")
     app.group_creation.open_groups_page()
     app.group_creation.init_edit_first_group()
