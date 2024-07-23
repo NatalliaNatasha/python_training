fa9f0da844b1f51902afb303305818185a2c1fff
Natalia Zen <natallia_zen@senla.eu>
2024-07-23 23:29:04 +0300

    Fixed

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,11 +1,10 @@
 from model.contact import Contact
 
 def test_edit_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact_creation.go_to_home_page()
     app.contact_creation.select_edit_button(number="9")
     app.contact_creation.fill_form(Contact(firstname="why"))
     app.contact_creation.update_contact()
-    app.session.logout()
+
 
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,9 +1,7 @@
 from model.group import Group
 
 def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
     app.group_creation.open_groups_page()
     app.group_creation.init_edit_first_group()
     app.group_creation.fill_group_form(Group(name="edit", footer="group"))
     app.group_creation.submit_group_edit()
-    app.session.logout()
