56337af16dfea9599e3b59bea52128919b23d15c
Natalia Zen <natallia_zen@senla.eu>
2024-08-10 00:02:38 +0300

    Проверка списков

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -17,8 +17,8 @@ def test_edit_contact(app):
     contact.id=old_contacts[0].id
     new_contacts = app.helper_contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    #old_contacts[0]=contact
-    #assert sorted(old_contacts,key=Contact.id_or_max)==sorted(new_contacts,key=Contact.id_or_max)
+    old_contacts[0]=contact
+    assert sorted(old_contacts,key=Contact.id_or_max)==sorted(new_contacts,key=Contact.id_or_max)
 
 
 
