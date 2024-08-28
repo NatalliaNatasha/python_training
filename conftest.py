07f97d81ab19ab2b84af86fd2096efb0b428a4a3
Natalia Zen <natallia_zen@senla.eu>
2024-08-28 23:05:18 +0300

    Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -7,11 +7,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    baseUrl = request.config.getoption("--baseUrl")
+    browser = request.config.getoption("--browser")
     if fixture is None:
-        fixture=Application()
+        fixture=Application(browser=browser,baseUrl=baseUrl)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser,baseUrl=baseUrl)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -23,4 +25,7 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store",default="firefox")
+    parser.addoption("--baseUrl", action="store", default="https://localhost/addressbook/")
 
--- fixture/application.py
+++ fixture/application.py
@@ -4,12 +4,20 @@ from fixture.helper_group import HelperGroup
 from fixture.helper_contact import HelperContact
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self,browser,baseUrl):
+        if browser=="firefox":
+            self.wd = webdriver.Firefox()
+        elif browser=="chrome":
+            self.wd = webdriver.Chrome()
+        elif browser=="ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(5)
         self.session=SessionHelper(self)
         self.helper_group=HelperGroup(self)
         self.helper_contact = HelperContact(self)
+        self.baseUrl=baseUrl
 
     def is_valid(self):
         try:
@@ -20,7 +28,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("https://localhost/addressbook/")
+        wd.get(self.baseUrl)
 
 
     def destroy(self):
