984d2d8b4249d2ea8b16c4fab62fe2815f2724ad
Natalia Zen <natallia_zen@senla.eu>
2024-09-29 18:26:20 +0300

    Задание №20: Переделать тесты, чтобы сравнивались данные, загруженные из БД-fixed

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -23,8 +23,9 @@ def test_edit_contact(app,db,check_ui):
     new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     old_contacts = [contact_new if contact.id == id_random else contact for contact in old_contacts]
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
-        assert sorted(old_contacts,key=Contact.id_or_max)==sorted(new_contacts,key=Contact.id_or_max)
+        assert sorted(new_contacts, key=Contact.id_or_max)==sorted(app.helper_group.get_contact_list(),key=Contact.id_or_max)
 
 
 # def test_edit_contact(app):
